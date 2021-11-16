535ab49c0ae46409b13e4c9157d4122228b04629
Zee Hussain <zhussain.mba@gmail.com>
2021-11-16 00:37:58 -0700

    Created Models for Store, Tags, and Likes apps

--- likes/models.py
+++ likes/models.py
@@ -1,18 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 
-
-class Tags(models.Model):
-    label = models.CharField(max_length=255)
-
-
-class TaggedItem(models.Model):
-    tag = models.ForeignKey(Tags, on_delete=models.CASCADE)
-    # type of the content (video, article, product or music)
-    # ID of the product
+class LikedItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
